4ad43717f03476ae6c07f4eca3d6b3e7f346b73b
jeremyMann-dev <79615986+jeremyMann-dev@users.noreply.github.com>
2021-03-27 21:39:03 +1100

    navMenu.js update

--- tailwindTest/build/script/navMenu.js
+++ tailwindTest/build/script/navMenu.js
@@ -1,8 +1,12 @@
 // grab everything we need
 const mobileMenuButton = document.querySelector(".mobile-menu-button");
 const sidebar = document.querySelector(".sidebar");
+const mobileCloseButton = document.querySelector(".mobile-close-button");
 
 // add our event listener for the click
 mobileMenuButton.addEventListener("click", () => {
   sidebar.classList.toggle("-translate-x-full");
+});
+mobileCloseButton.addEventListener("click", () => {
+    sidebar.classList.toggle("-translate-x-full")
 });
